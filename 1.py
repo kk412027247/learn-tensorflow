50bdfc98581387a9dd304ca436614da565d66c43
TMD <kk412027247@gmail.com>
2019-06-20 00:36:02 +0800

    hello tensorflow

---
+++ 1.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+# import numpy as np
+# import matplotlib.pyplot as plt
+#
+# train_X = np.linspace(-1, 1, 100)
+# train_Y = 2 * train_X + np.random.random(*train_X.shape) * 0.3
+# plt.plot(train_X, train_Y, 'ro', label='Original data')
+# plt.legend()
+# plt.show()
+#
+# X = tf.placeholder('float')
+# Y = tf.placeholder('float')
+#
+# W = tf.Variable(tf.random_normal([1]), name='weight')
+# b = tf.Variable(tf.zeros([1]), name='bias')
+#
+# z = tf.multiply(X, W) + b;
+
+print(tf)
+
+
