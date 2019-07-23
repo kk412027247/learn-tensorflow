c40e31c32ac5407ea1fb3555cdfe60fd299b0f44
TMD <kk412027247@gmail.com>
2019-07-24 07:28:27 +0800

    hello

---
+++ 2.py
@@ -0,0 +1,10 @@
+from __future__ import print_function
+
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+print(mnist)
+
+
+# import tensorflow as tf
+
