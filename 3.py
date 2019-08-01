88bc1d1096d239944ccd4578d0f58e09c87b94a2
tmd <kk412027247@gmail.com>
2019-08-01 20:58:48 +0800

    intro to pandas

---
+++ 3.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+import numpy as np
+
+x_data = np.random.rand(100).astype(np.float32)
+y_data = x_data * 0.1 + 0.3
+
+Weights = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+biases = tf.Variable(tf.zero([1]))
+
+y = Weights * x_data + biases
+
+loss = tf.reduce_mean(tf.square(y-y_data))
+optimizer = tf.train.GradientDescentOptimizer(0.5)
+train = optimizer.minimize(loss)
