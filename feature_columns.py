a622e61078fdbcb235c8c58d57c69ed69c2c88cb
TMD <kk412027247@gmail.com>
2019-08-25 22:52:24 +0800

    feature columns

---
+++ feature_columns.py
@@ -0,0 +1,50 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+from tensorflow import feature_column
+from tensorflow.keras import layers
+import tensorflow_hub as hub
+
+from sklearn.model_selection import train_test_split
+
+URL = 'https://storage.googleapis.com/applied-dl/heart.csv'
+
+dataframe = pd.read_csv(URL)
+print(dataframe.head())
+
+train, test = train_test_split(dataframe, test_size=0.2)
+train, val = train_test_split(train, test_size=0.2)
+print(len(train), 'train examples')
+print(len(val), 'validation examples')
+print(len(test), 'test examples')
+
+
+def df_to_dataset(dataframe, shuffle=True, batch_size=32):
+    dataframe = dataframe.copy()
+    labels = dataframe.pop('target')
+    ds = tf.data.Dataset.from_tensor_slices((dict(dataframe), labels))
+    if shuffle:
+        ds = ds.shuffle(buffer_size=len(dataframe))
+    ds = ds.batch(batch_size)
+    return ds
+
+
+batch_size = 5
+train_ds = df_to_dataset(train, batch_size=batch_size)
+val_ds = df_to_dataset(val, shuffle=False, batch_size=batch_size)
+test_ds = df_to_dataset(test, shuffle=False, batch_size=batch_size)
+
+for feature_batch, label_batch in train_ds.take(1):
+    print('Every feature:', list(feature_batch.keys()))
+    print('A batch of ages:', feature_batch['age'])
+    print('A batch of targets:', label_batch)
+
+example_batch = next(iter(train_ds))[0]
+
+
+def demo(feature_column):
+    feature_layer = layers.DenseFeatures(feature_column)
+    print(feature_layer(example_batch).numpy())
