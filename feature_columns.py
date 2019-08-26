08fb4b937bc92126680fbcd7db538340dd4548dc
tmd <kk412027247@gmail.com>
2019-08-26 19:32:30 +0800

    feature_columns

--- feature_columns.py
+++ feature_columns.py
@@ -3,12 +3,17 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-
 from tensorflow import feature_column
 from tensorflow.keras import layers
 import tensorflow_hub as hub
-
 from sklearn.model_selection import train_test_split
+import os
+
+proxy = 'http://127.0.0.1:1080'
+os.environ['http_proxy'] = proxy
+os.environ['HTTP_PROXY'] = proxy
+os.environ['https_proxy'] = proxy
+os.environ['HTTPS_PROXY'] = proxy
 
 URL = 'https://storage.googleapis.com/applied-dl/heart.csv'
 
@@ -48,3 +53,65 @@ example_batch = next(iter(train_ds))[0]
 def demo(feature_column):
     feature_layer = layers.DenseFeatures(feature_column)
     print(feature_layer(example_batch).numpy())
+
+
+age = feature_column.numeric_column('age')
+demo(age)
+
+age_buckets = feature_column.bucketized_column(age, boundaries=[18, 25, 30, 35, 40, 45, 50, 55, 60, 65])
+demo(age_buckets)
+
+thal = feature_column.categorical_column_with_vocabulary_list(
+    'thal', ['fixed', 'normal', 'reversible']
+)
+
+thal_one_hot = feature_column.indicator_column(thal)
+demo(thal_one_hot)
+
+thal_embedding = feature_column.embedding_column(thal, dimension=8)
+demo(thal_embedding)
+
+thal_hashed = feature_column.categorical_column_with_hash_bucket('thal', hash_bucket_size=1000)
+demo(feature_column.indicator_column(thal_hashed))
+
+crossed_feature = feature_column.crossed_column([age_buckets, thal], hash_bucket_size=1000)
+demo(feature_column.indicator_column(crossed_feature))
+
+feature_columns = []
+
+for header in ['age', 'trestbps', 'chol', 'thalach', 'oldpeak', 'slope', 'ca']:
+    feature_columns.append(feature_column.numeric_column(header))
+
+feature_columns.append(age_buckets)
+
+feature_columns.append(thal_one_hot)
+
+feature_columns.append(thal_embedding)
+
+crossed_feature = feature_column.indicator_column(crossed_feature)
+feature_columns.append(crossed_feature)
+
+feature_layer = tf.keras.layers.DenseFeatures(feature_columns)
+batch_size = 32
+train_ds = df_to_dataset(train, batch_size=batch_size)
+val_ds = df_to_dataset(val, shuffle=False, batch_size=batch_size)
+test_ds = df_to_dataset(test, shuffle=False, batch_size=batch_size)
+
+model = tf.keras.Sequential([
+    feature_layer,
+    layers.Dense(128, activation='relu'),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'],
+              run_eagerly=True)
+
+model.fit(train_ds,
+          validation_data=val_ds,
+          epochs=5)
+
+loss, accuracy = model.evaluate(test_ds)
+print('Accuracy', accuracy)
