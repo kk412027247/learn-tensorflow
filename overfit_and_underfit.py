2f6c71650d6ef81532ed232ec54d8e911e6b9f5e
tmd <kk412027247@gmail.com>
2019-08-30 21:58:37 +0800

    plt.show

---
+++ overfit_and_underfit.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+NUM_WORDS = 1000
+
+(train_data, train_labels), (test_data, test_labels) = keras.datasets.imdb.load_data(num_words=NUM_WORDS)
+
+
+def multi_hot_sequences(sequences, dimension):
+    results = np.zeros((len(sequences), dimension))
+    for i, word_indices in enumerate(sequences):
+        results[i, word_indices] = 1.0
+    return results
+
+
+train_data = multi_hot_sequences(train_data, dimension=NUM_WORDS)
+test_data = multi_hot_sequences(test_data, dimension=NUM_WORDS)
+
+plt.plot(train_data[0])
+plt.show()
