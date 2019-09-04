975cb426fdf58773a13cc95aaec2a6e8abb08071
tmd <kk412027247@gmail.com>
2019-09-04 19:53:36 +0800

    model.fit

--- transfer_learning.py
+++ transfer_learning.py
@@ -54,3 +54,57 @@ for image_batch, label_batch in train_batches.take(1):
     pass
 
 print(image_batch.shape)
+
+IMG_SIZE = (IMG_SIZE, IMG_SIZE, 3)
+
+base_model = tf.keras.applications.MobileNetV2(input_shape=IMG_SIZE,
+                                               include_top=False,
+                                               weights='imagenet')
+
+feature_batch = base_model(image_batch)
+print(feature_batch.shape)
+
+base_model.trainable = False
+base_model.summary()
+
+global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
+feature_batch_average = global_average_layer(feature_batch)
+print(feature_batch_average.shape)
+
+prediction_layer = keras.layers.Dense(1)
+prediction_batch = prediction_layer(feature_batch_average)
+print(prediction_batch.shape)
+
+model = keras.Sequential([
+    base_model,
+    global_average_layer,
+    prediction_layer
+])
+
+base_learning_rate = 0.0001
+
+model.compile(optimizer=tf.keras.optimizers.RMSprop(lr=base_learning_rate),
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+model.summary()
+
+print(len(model.trainable_variables))
+
+num_train, num_val, num_test = (
+    metadata.splits['train'].num_examples * weight / 10
+    for weight in SPLIT_WEIGHTS
+)
+
+initial_epochs = 10
+steps_per_epoch = round(num_train)
+validation_steps = 20
+
+loss0, accuracy0 = model.evaluate(validation_batches, steps=validation_steps)
+
+print('initial loss: {:.2f}'.format(loss0))
+print('initial accuracy: {:.2f}'.format(accuracy0))
+
+history = model.fit(train_batches,
+                    epochs=initial_epochs,
+                    validation_data=validation_batches)
